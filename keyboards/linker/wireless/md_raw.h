019e3f2be4a5db906c05520c24c2924e21f1c2c3
toyman <toyman@andestech.com>
2025-04-24 04:32:42 +0000

    Remove default config.h for jiebao l49 keymap and update md_raw.h to comment out a replaced HID send definition while adding new ones.

--- keyboards/linker/wireless/md_raw.h
+++ keyboards/linker/wireless/md_raw.h
@@ -6,5 +6,8 @@
 #define RENAME_WITH_LINE(A, B) COMBINE(A, B)
 #define COMBINE(A, B) A##B
 #define raw_hid_send(a, b) RENAME_WITH_LINE(_temp_rhs_, __LINE__)(a, b)
+
 #define _temp_rhs_29 replaced_hid_send  // raw_hid.h
-#define _temp_rhs_461 replaced_hid_send // via.c
+// #define _temp_rhs_461 replaced_hid_send // via.c
+#define _temp_rhs_465 replaced_hid_send // via.c
+#define _temp_rhs_441 replaced_hid_send // via.c
